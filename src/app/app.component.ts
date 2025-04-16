92207b8c4f7d7b344243f0f23470106c6ad7e809
Christian Gabor <chrisgabor95@gmail.com>
2025-04-15 21:51:43 -0300

    Root y movimiento basico para testeo agregado

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,8 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterLink, RouterOutlet, RouterLinkActive } from '@angular/router';
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, RouterLink, RouterLinkActive],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { NoticiasUserComponent } from './noticias/noticias-user/noticias-user.component';
+import { NoticiasAdminComponent } from './noticias/noticias-admin/noticias-admin.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'noticias-users', component: NoticiasUserComponent },
+  { path: 'noticias-admin', component: NoticiasAdminComponent },
+];
