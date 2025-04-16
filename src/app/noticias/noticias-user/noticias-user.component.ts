1478005b98880a69257c98d1b53e5543363d384b
Christian Gabor <chrisgabor95@gmail.com>
2025-04-16 17:29:17 -0300

    vista de usuario completa

--- src/app/noticias/noticias-user/noticias-user.component.ts
+++ src/app/noticias/noticias-user/noticias-user.component.ts
@@ -1,11 +1,42 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { BbddService } from '../../../bbdd.service';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { ModalComponent } from '../modal/modal.component';
 
 @Component({
   selector: 'app-noticias-user',
   imports: [],
   templateUrl: './noticias-user.component.html',
-  styleUrl: './noticias-user.component.css'
+  styleUrl: './noticias-user.component.css',
 })
 export class NoticiasUserComponent {
-
+  private modalService = inject(NgbModal);
+  eventos: any;
+  constructor(private bbddservice: BbddService) {
+    this.getEventos();
+  }
+  async getEventos() {
+    this.bbddservice.getDatabase('noticiasYeventos').subscribe((data) => {
+      this.eventos = data;
+      this.getImages();
+    });
+  }
+  async getImages() {
+    this.eventos.forEach((evento: any) => {
+      this.bbddservice
+        .getImage(evento.s3key)
+        .subscribe((data) => {
+          evento['s3key'] =
+            'data:image/png;base64,' + data['data' as keyof typeof data];
+        });
+    });
+  }
+  open(eventCard: HTMLElement, evento: any) {
+    eventCard.scrollIntoView({ behavior: 'smooth' });
+    const modalRef = this.modalService.open(ModalComponent, {
+      size: 'xl',
+      centered: true,
+    });
+    modalRef.componentInstance.evento = evento;
+  }
 }
--- src/bbdd.service.ts
+++ src/bbdd.service.ts
@@ -8,9 +8,18 @@ export class BbddService {
 
   constructor(private http: HttpClient) { }
   xmlReq = new XMLHttpRequest();
-  url = "https://nt876k2q7f.execute-api.us-east-1.amazonaws.com/"
-  getImage(foldername: string, filename: string) {
-    return this.http.post(`${this.url}/getphoto?folder=${foldername}&image=${filename}`, {})
+  url = "https://nt876k2q7f.execute-api.us-east-1.amazonaws.com"
+  getDababases() {
+    return this.http.get(`${this.url}/tablas`)
+  }
+  getDatabase(basename: string) {
+    return this.http.get(`${this.url}/tablas/${basename}`)
+  }
+  getElementByIdFromDatabase(basename: string, id: string) {
+    return this.http.get(`${this.url}/tablas/${basename}/${id}`)
+  }
+  getImage(s3key: string) {
+    return this.http.post(`${this.url}/getphoto?s3key=${s3key}`, {})
   }
   saveImage(foldername: string, filename: string, imageData: string) {
     return this.http.post(`${this.url}/savephoto`, JSON.stringify({"folder": foldername, 'name': filename, "data": imageData}));
