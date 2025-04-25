e0831eea2bb16ca4d073d578db9ac89265a62131
Norberto Juárez <96215830+norbertojuarez@users.noreply.github.com>
2025-04-25 19:34:38 -0300

    Login (#2)
    * Login y dashboards conectados a cognito
    
    * mejora en CSS para home responsive

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,26 @@
 import { Routes } from '@angular/router';
+import { AdminDashboardComponent } from './pages/admin-dashboard/admin-dashboard.component';
+import { UserDashboardComponent } from './pages/user-dashboard/user-dashboard.component';
+import { LoginComponent } from './pages/login/login.component';
+import { AdminGuard } from './guards/admin.guard';
+import { UserGuard } from './guards/user.guard';
 
-export const routes: Routes = [];
+
+export const routes: Routes = [
+    { path: '', component: LoginComponent },
+    {
+      path: 'admin-dashboard',
+      component: AdminDashboardComponent,
+      canActivate: [AdminGuard],
+    },
+    {
+      path: 'user-dashboard',
+      component: UserDashboardComponent,
+      canActivate: [UserGuard],
+    },
+    {
+      path: 'login',
+      component: LoginComponent,
+    },   
+    { path: '**', redirectTo: '/login' }
+  ];
