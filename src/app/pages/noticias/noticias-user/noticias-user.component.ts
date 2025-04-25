3f0cb370211561e8acf174868aac8d0e367383d1
Norberto Juárez <96215830+norbertojuarez@users.noreply.github.com>
2025-04-25 19:53:44 -0300

    Noticias eventos (#4)
    * Base de noticias + servicio de imagenes
    
    * Limpieza del root
    
    * Root y movimiento basico para testeo agregado
    
    * vista de usuario completa
    
    * arreglo menor
    
    * Funcionamiento con mock la vista de usuario
    
    * Noticias admin y users completas
    
    * updated .gitignore
    
    * archivo de testeo eliminado
    
    * Eliminacion del campo "tipo" y aclaraciones en funcines del servicio
    
    * Reestructuracion de las carpetas
    
    * Agregado responsive
    
    ---------
    
    Co-authored-by: Christian Gabor <chrisgabor95@gmail.com>

---
+++ src/app/pages/noticias/noticias-user/noticias-user.component.ts
@@ -0,0 +1,59 @@
+import { Component, inject } from '@angular/core';
+import { BbddService } from '../../../services/bbdd.service';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { ModalComponent } from '../modal/modal.component';
+
+import * as noticias from '../noticias.json'
+
+@Component({
+  selector: 'app-noticias-user',
+  imports: [],
+  templateUrl: './noticias-user.component.html',
+  styleUrl: './noticias-user.component.css',
+})
+export class NoticiasUserComponent {
+  private modalService = inject(NgbModal);
+  eventos: any;
+  eventosFiltrados: any;
+  constructor(private bbddservice: BbddService) {
+    this.getEventos();
+  }
+  async getEventos() {
+    this.eventos = noticias.datos
+    this.eventosFiltrados = this.eventos
+    // this.bbddservice.getDatabase('noticiasYeventos').subscribe((data) => {
+    //   this.eventos = data;
+    //   this.getImages();
+    // });
+  }
+  async getImages() {
+    this.eventos.forEach((evento: any) => {
+      this.bbddservice
+        .getImage(evento.s3key)
+        .subscribe((data) => {
+          evento['s3key'] =
+            'data:image/png;base64,' + data['data' as keyof typeof data];
+        });
+    });
+  }
+  open(evento: any) {
+    const modalRef = this.modalService.open(ModalComponent, {
+      size: 'xl',
+      centered: true,
+    });
+    modalRef.componentInstance.evento = evento;
+  }
+  filtrar(event: any) {
+    let busqueda = event.target.value.toLowerCase()
+    if (busqueda == '') {
+      this.eventosFiltrados = this.eventos;
+    } else {
+      this.eventosFiltrados = []
+      this.eventos.forEach((evento: any) => {
+        if (evento['titulo'].toLowerCase().includes(busqueda) || evento['resumen'].toLowerCase().includes(busqueda) || evento['descripcion'].toLowerCase().includes(busqueda)) {
+          this.eventosFiltrados.push(evento)
+        }
+      })
+    }
+  }
+}
