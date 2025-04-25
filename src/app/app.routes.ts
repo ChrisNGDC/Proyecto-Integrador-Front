88015b0674b93a9d34e9beea65307b05683bd8ce
guadalupe sirio <guadalupe.sirio@gmail.com>
2025-04-25 19:07:53 -0300

    Add components perfil and egresados

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
+import { EgresadosComponent } from './components/egresados/egresados.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { 
+    path: 'egresados', 
+    component: EgresadosComponent 
+  },
+  { 
+    path: 'perfil', 
+    loadComponent: () => import('./components/perfil/perfil.component').then(m => m.PerfilComponent)
+  },
+  { path: '', redirectTo: 'egresados', pathMatch: 'full' },
+  { path: '**', redirectTo: 'egresados' }
+];
