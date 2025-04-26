1253a43a7afba3b91df92a76ec6c4d3f5e8c8768
Christian Gabor <chrisgabor95@gmail.com>
2025-04-26 11:54:13 -0300

    Organizacion de componentes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,16 @@
 import { Component } from "@angular/core";
 import { RouterModule } from "@angular/router";
 import { CommonModule } from "@angular/common";
-import { NavbarComponent } from "./components/navbar/navbar.component";
 
 @Component({
 
   selector: "app-root",
   standalone: true,
-  imports: [CommonModule, RouterModule, NavbarComponent],
+  imports: [CommonModule, RouterModule],
   templateUrl: "./app.component.html",
   styleUrls: ["./app.component.css"]
 })
 export class AppComponent {
   title = 'Frontend';
-  
+
 }
