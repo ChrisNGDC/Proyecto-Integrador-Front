77647719ccfea888e9e469d291063bc76ac66b60
Christian Gabor <chrisgabor95@gmail.com>
2025-04-26 10:44:24 -0300

    Módulo de oportunidades (#6)
    * Módulo de oportunidades
    
    Co-authored-by: MarinelaMontilla <marinelamontillamendez@gmail.com>

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,9 @@
-import { Routes } from '@angular/router';
-import { AdminDashboardComponent } from './pages/admin-dashboard/admin-dashboard.component';
-import { UserDashboardComponent } from './pages/user-dashboard/user-dashboard.component';
-import { LoginComponent } from './pages/login/login.component';
-import { AdminGuard } from './guards/admin.guard';
-import { UserGuard } from './guards/user.guard';
+import { Routes } from "@angular/router";
+import { OpportunitiesComponent } from "./components/opportunities/opportunities.component";
+import { ProfileComponent } from "./components/profile/profile.component";
+import { NewsComponent } from "./components/news/news.component";
+import { SurveysComponent } from "./components/surveys/surveys.component";
+import { AdminOpportunitiesComponent } from "./components/admin-opportunities-component/admin-opportunities-component.component";
 
 
 export const routes: Routes = [
